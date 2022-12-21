33d05807b73349b437e2f7a6b9bc26e6d61c6387
getwlad <wladmir.rrodrigues@gmail.com>
2022-12-21 10:11:26 -0300

    rail

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes";
 import "./database/index";
 import "dotenv/config";
+const cors = require("cors");
 class App {
   constructor() {
     this.server = express();
@@ -11,19 +12,7 @@ class App {
   middlewares() {
     this.server.use(express.json());
     this.server.use("/uploads", express.static("uploads"));
-    this.server.use((req, res, next) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Credentials", "true");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,OPTIONS,POST,PUT, DELETE"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-      );
-      next();
-    });
+    this.server.use(cors());
   }
   routes() {
     this.server.use(routes);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 import app from "./app";
 require("dotenv/config");
 
-app.listen(process.env.PORT || 3001, "0.0.0.0");
+app.listen(process.env.API_PORT || process.env.PORT || 3001, "0.0.0.0");
